bc1d4f2fbf1b7452b25824013726e68c2c58d333
blixtcorp <jakobpersson007@gmail.com>
2021-12-02 01:24:52 +0100

    improve structure

--- src/Day1.java
+++ src/Day1.java
@@ -6,20 +6,19 @@ public class Day1 {
 
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<Integer> data = new ArrayList<>();
-        File numbers = new File("src/myData");
+        File numbers = new File("src/resources/myData");
         Scanner input = new Scanner(numbers);
 
         while(input.hasNextLine()){
             data.add(Integer.parseInt(input.nextLine()));
         }
-
         partA(data);
         partB(data);
     }
 
     static void partA(ArrayList<Integer> data) {
-
         int result = 0;
+
         for(int i = 1; i < data.size(); i++){
         //checking recursively to dodge nullpointers
             if(data.get(i) > data.get(i-1)){
@@ -33,6 +32,7 @@ public class Day1 {
         int result = 0;
         int currentValue;
         int previousValue = data.get(0) + data.get(1) + data.get(2);
+
         for(int i = 1; i < data.size() - 2; i++){
             currentValue = data.get(i) + data.get(i+1) + data.get(i+2);
             if(currentValue - previousValue > 0){
